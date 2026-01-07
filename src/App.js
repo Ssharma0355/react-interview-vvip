77d3517755acbb5c9092aafc37d6dd8f0559abde
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-07 21:53:56 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UseStateHook from './hooks/UseStateHook'
 
 const App = () => {
+  const[state, setState] = useState(false)
+  const showUseState =()=>{
+    setState(!state)
+  }
   return (
     <div>
+      <h1>Hooks</h1>
+      <p>useState Hook</p>
+      <UseStateHook />
+      
+
       
     </div>
   )
