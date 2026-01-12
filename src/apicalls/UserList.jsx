bde3388c85cabcd6f8fd57ad140c698dcd7a29a9
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-12 16:57:24 +0530

    fixed isse

--- src/apicalls/UserList.jsx
+++ src/apicalls/UserList.jsx
@@ -3,8 +3,26 @@ import axios from 'axios';
 
 const UserList = () => {
     const [userData, setUserData] = useState([]);
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("");
+    const [search, setSearch] = useState("");
+    const [postData, setPostData] = useState(null);
     
 
+
+    const addPost = async ()=>{
+        try{
+            const response = await axios.post("https://jsonplaceholder.typicode.com/posts",{
+                title,body
+            })
+            setPostData(response.data)
+        }
+        catch(err){
+            console.log(err)
+        }
+      
+    }
+ 
     useEffect(()=>{
         const getUser = async ()=>{
         axios.get("https://jsonplaceholder.typicode.com/posts")
@@ -13,8 +31,26 @@ const UserList = () => {
         getUser();
     },[])
 
+
   return (
     <div>
+        <input type="text" value={search} onChange={(e)=> setSearch(e.target.value)} />
+    
+            <h1>Add Title</h1>
+            <input type="text" value={title} onChange={(e)=> setTitle(e.target.value)} />
+            <br />
+            <h1>Add Body</h1>
+            <input type="text" value={body} onChange={(e)=> setBody(e.target.value)} />
+            <br />
+            <button onClick={addPost}>Add Post</button>
+            <br />
+
+            {postData && (
+                <div>
+                    <h1>{postData.id}</h1>
+                </div>
+            )}
+         
         <ul>
             User Data
             {userData.map((user)=>(
