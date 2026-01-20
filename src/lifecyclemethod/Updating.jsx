9109b90a2d786256d67691d1086d9391e8d935a2
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-20 19:44:11 +0530

    updation

--- src/lifecyclemethod/Updating.jsx
+++ src/lifecyclemethod/Updating.jsx
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Updating extends Component {
-    componentDidUpdate(prevstate, preprop){
-        if(preprop != prevstate){
-            console.log("Component updated")
-        }
-        else{
-            console.log("Not updated")
-        }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps.number);
+    console.log(this.props.number);
+
+    if (prevProps.number !== this.props.number) {
+      console.log("Component updated");
+    } else {
+      console.log("Not updated");
     }
+  }
+
   render() {
     return (
       <div>
         <h1>{this.props.number}</h1>
         hehe
       </div>
-    )
+    );
   }
 }
