924387677a156746ca6f2b6309a088cf3c54f4d3
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-31 11:54:49 +0530

    2nd method to use HOC

--- src/HighOrderComponent/AppFile.js
+++ src/HighOrderComponent/AppFile.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { WithAuth } from './utils/WithAuth'
 import Dashboard from './Dashboard'
+import AuthDashboard from './Dashboard'
 
 const AppFile = () => {
     const CheckAuth = WithAuth(Dashboard)
   return (
     <div>
-      <CheckAuth />
+      {/* <CheckAuth /> */}
+      
+
+      {/* 2nd method to use  */}
+      <AuthDashboard />
     </div>
   )
 }
