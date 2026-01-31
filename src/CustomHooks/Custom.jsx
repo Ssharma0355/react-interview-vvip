e600de39796cf9e4335c3eaea2ed4501b467422e
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-31 13:57:13 +0530

    sidebar in custom hooks

--- src/CustomHooks/Custom.jsx
+++ src/CustomHooks/Custom.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 import { useToggle } from './useToggle'
+import { useSidebar } from './useSidebar';
 
 const Custom = () => {
     const [value, toggle] = useToggle(false);
+
+    const [isOpen, side] = useSidebar(false);
   return (
     <div>
         <button onClick={toggle}>modal</button>
         {value ? "Open":"close"}
+
+        <br />
+        <button onClick={side}>Sidebar</button>
+        {isOpen ? "Sidebar open":"Sidebar close"}
     </div>
   )
 }
