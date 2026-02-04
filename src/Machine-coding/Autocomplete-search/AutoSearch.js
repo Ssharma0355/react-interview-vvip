08f4b285a123f90638c55d21d6bfcf0aa27ecf52
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-04 10:41:59 +0530

    added functionality

--- src/Machine-coding/Autocomplete-search/AutoSearch.js
+++ src/Machine-coding/Autocomplete-search/AutoSearch.js
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from 'react'
+import "./Autostyle.css"
 
 const AutoSearch = () => {
     const [input, setInput] = useState("");
-    const [results, setResults] = useState([])
+    const [results, setResults] = useState([]);
+    const [showResult, setShowResults] = useState(false);
 
     const fetchData = async()=>{
         const data = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
         const jsonData = await data.json();
         setResults(jsonData.recipes)
     }
+    const debounce =()=>{ setTimeout(()=>{
+        fetchData();
+    },3000);
+}
 
     useEffect(()=>{
-        fetchData();
+        debounce();
     },[input]);
 
-    
   return (
     <div>
-        <input type='text' value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <input className='search-input' type='text' value={input} onChange={(e)=>setInput(e.target.value)} onFocus={()=>setShowResults(true)} onBlur={()=>setShowResults(false)}/>
         <div>
-            {results.map((result)=>(
-                <li key={result.id}>{result.name}</li>
-            ))}
+                {showResult && 
+                <div className='results'>
+                   {results.map((result)=>(
+                    <li className='result' key={result.id}>{result.name}</li>
+                ))}
+                  </div>
+                }
         </div>
     </div>
   )
