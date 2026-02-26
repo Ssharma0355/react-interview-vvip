236c68cae85e06b6ff6e8cb7090c87794cfd58a0
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-26 09:22:58 +0530

    Traffic light issue fixed

---
+++ src/revise-mc/Trafficlisgt/TraficLight.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+const TraficLight = ({config=[]}) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    useEffect(()=>{
+        if(!config.length) return;
+
+        const timer = setTimeout(()=>{
+            setActiveIndex((prev)=>(prev+1) % config.length)
+        },config[activeIndex].duration)
+
+        return () => clearTimeout(timer)
+    },[activeIndex, config])
+  return (
+    <div style={
+        {
+            backgroundColor:"grey",
+            height:"200px",
+            width:"200px",
+            border:"12px"
+        }
+    }>
+        {config.map((light, index)=>(
+            <div key={index} 
+            style={{
+                height:"50px",
+                margin:"9px",
+                width:"50px",
+                borderRadius:"50%",
+                backgroundColor: index === activeIndex ? light.color : "#555"
+            }}
+            >
+
+            </div>
+        ))}
+      
+    </div>
+  )
+}
+
+export default TraficLight
